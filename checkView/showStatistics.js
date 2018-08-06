f83b54a2482ab7041551a28f9724c5a8f088e69d
liupp <liupp@2345.com>
2018-08-06 16:43:36 +0800

    modify

--- checkView/showStatistics.js
+++ checkView/showStatistics.js
@@ -3,7 +3,8 @@
     /*
       {
       doms: array, // 要检测的dom集合
-      canHideByContainer: false, // 是否可能超出父容器而被隐藏
+      canHideByContainer: false, // 是否会被父元素隐藏（因滚动或其他原因超出父元素范围而无法展示，不算展示）
+      container: dom, // 父容器， canHideByContainer为true时，必填
       callback: fn, // 展现后回调
       showSpace: int // 发展现时距离底部距离
     }
@@ -12,15 +13,26 @@
     this.canHideByContainer = params.canHideByContainer
     this.callback = params.callback
     this.showSpace = params.showSpace || 0
+    this.container = params.container
     var _this = this
     if (window.addEventListener) {
       window.addEventListener('scroll', function () {
         _this.checkShow()
       })
-    } else {
+      if (this.container) {
+        this.container.addEventListener('scroll', function () {
+          _this.checkShow()
+        })
+      }
+    } else if (window.attachEvent) {
       window.attachEvent('onscroll', function () {
         _this.checkShow()
       })
+      if (this.container) {
+        this.container.attachEvent('onscroll', function () {
+          _this.checkShow()
+        })
+      }
     }
   }
   ViewCheck.prototype = {
@@ -42,7 +54,7 @@
       return getOffset(dom)
     },
     addDom: function (doms) {
-      this.doms.concat(doms)
+      this.doms = this.doms.concat(doms)
     },
     isInView: function (dom) {
       var scrollT1, scrollT2
@@ -52,19 +64,24 @@
       scrollT2 = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
       clientHeight2 = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
       offsetT2 = this.offset(dom).top
-
       if (this.canHideByContainer) {
-        scrollT1 = dom.offsetParent.scrollTop
-        clientHeight1 = dom.offsetParent.clientHeight
+        scrollT1 = dom.parentNode.scrollTop
+        clientHeight1 = dom.parentNode.clientHeight
         offsetT1 = dom.offsetTop
-
-        if (scrollT1 + clientHeight1 >= offsetT1 + this.showSpace && scrollT2 + clientHeight2 >= offsetT2 -  scrollT1 + showSpace) {
-          isInView = true
+        // 未在父元素中被遮挡算展现
+        if (scrollT1 + clientHeight1 >= offsetT1 + this.showSpace &&
+          scrollT2 + clientHeight2 >= offsetT2 -  scrollT1 + this.showSpace) {
+           isInView = true
         }
       } else {
+        // 滚动超出屏幕底部算展现
         if (scrollT2 + clientHeight2 >= offsetT2 + this.showSpace) {
           isInView = true
         }
+      }
+      // 隐藏时不算展现
+      if (dom.offsetWidth === 0) {
+        isInView = false
       }
       return isInView
     },
